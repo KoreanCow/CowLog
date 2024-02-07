b54aaae26eee030b99f18db4a90d9299a6373d9f
KoreanCow <>
2024-02-07 18:53:22 +0900

    use reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import './App.scss'
 import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
 import RootPage from './pages/RootPage';
 import NotFoundPage from './pages/NotFoundPage';
-import NavBar from './components/NavBar/NavBar';
+import AuthPage from './pages/AuthPage';
+import SignupPage from './pages/SignupPage';
+import MyPage from './pages/MyPage';
+
 function App() {
   return (
     <div className="body">
       <NavBar />
       <Routes>
         <Route path='/' element={<RootPage />} />
+        <Route path='/auth' element={<AuthPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/mypage' element={<MyPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </div>
