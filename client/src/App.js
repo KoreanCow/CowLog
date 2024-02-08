67f1f126c2c55c95e7917b8ed56520f33b1c4de7
KoreanCow <>
2024-02-08 18:51:42 +0900

    Refactor: Update import paths after folder relocation

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import './App.scss'
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
-import RootPage from './pages/RootPage';
-import NotFoundPage from './pages/NotFoundPage';
-import AuthPage from './pages/AuthPage';
-import SignupPage from './pages/SignupPage';
+
+import RootPage from './pages/RootPage/RootPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import AuthPage from './pages/AuthPage/index';
+import SignupPage from './pages/SignUpPage/index';
+import PostPage from './pages/PostPage/index';
 import MyPage from './pages/MyPage';
+
+import LoadingComponent from './components/Loading/LoadingComponent';
 
 function App() {
   return (
     <div className="body">
       <NavBar />
       <Routes>
-        <Route path='/' element={<RootPage />} />
+        <Route path='/' element={<RootPage LoadingComponent={LoadingComponent} />} />
         <Route path='/auth' element={<AuthPage />} />
         <Route path='/signup' element={<SignupPage />} />
+        <Route path='/posts' element={<PostPage />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
