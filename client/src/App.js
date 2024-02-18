943e12a4ac1905918faff57a864c587a2bf02447
KoreanCow <>
2024-02-18 16:17:56 +0900

    Feat: Add PostingPage.js Route

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ import RootPage from './pages/RootPage/RootPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import AuthPage from './pages/AuthPage/index';
 import SignupPage from './pages/SignUpPage/index';
-import PostPage from './pages/PostPage/index';
+import PostPage from './pages/PostPage/PostPage';
+import PostingPage from './pages/PostPage/PostingPage/PostingPage';
 import MyPage from './pages/MyPage';
 
 import LoadingComponent from './components/Loading/LoadingComponent';
@@ -19,7 +20,8 @@ function App() {
         <Route path='/' element={<RootPage LoadingComponent={LoadingComponent} />} />
         <Route path='/auth' element={<AuthPage />} />
         <Route path='/signup' element={<SignupPage />} />
-        <Route path='/posts' element={<PostPage />} />
+        <Route path='/posts' element={<PostPage LoadingComponent={LoadingComponent} />} />
+        <Route path='/posting' element={<PostingPage />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
