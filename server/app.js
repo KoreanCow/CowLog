cc91ead52cd13ec7c51754c93a7fee5215ba3825
KoreanCow <>
2024-02-18 16:28:27 +0900

    Feat: add api/posting Router

--- server/app.js
+++ server/app.js
@@ -10,16 +10,17 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const { MONGO_URI } = config
 
-import rootRoutes from './routes/api/index'
-import postRoutes from './routes/api/post'
-import userRoutes from './routes/api/user'
-import authRoutes from './routes/api/auth'
-import wishRoutes from './routes/api/wish'
+import rootRoutes from './routes/api/index' // 메인 페이지 라우터
+import postRoutes from './routes/api/post' // blog | posts 페이지
+import userRoutes from './routes/api/user' // 회원가입 페이지 라우터
+import authRoutes from './routes/api/auth' // 로그인 토큰 발급 라우터
+import wishRoutes from './routes/api/wish' // 위시리스트 관련 라우터
+import postingRoutes from './routes/api/posting' // 게시글 작성 라우터
 
 app.use(hpp());
 app.use(helmet());
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 app.use(morgan('dev'));
 
 app.use(express.json());
@@ -29,11 +30,14 @@ mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connecting Success'))
   .catch((e) => console.log(e));
 
-// Use routes
-// app.get('/');
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 app.use('/', rootRoutes);
 app.use('/api/post', postRoutes)
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/wish', wishRoutes);
+app.use('/api/posting', postingRoutes);
 export default app;
