e6ecc4ce4a4cf07fbea98018381768461f3e9432
KoreanCow <>
2024-02-21 15:40:39 +0900

    Feat: Added Wishlsit Component

--- client/src/components/BlogPosts/BlogPosts.js
+++ client/src/components/BlogPosts/BlogPosts.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { openmodal } from '../../redux/actions/modalActions';
 
-const BlogPosts = ({ posts, isLatest }) => {
-  // isLatest prop이 전달되지 않았거나 true일 때 최신 게시물을 렌더링
-  if (isLatest === undefined || isLatest) {
+const BlogPosts = ({ posts, type }) => {
+  const dispatch = useDispatch();
+  const handleClickPost = (postId) => {
+    console.log(postId);
+    dispatch(openmodal(postId));
+  }
+  if (type === 'latest') {
     return (
       <>
         {posts.map((post, index) => (
-          <div className='Post' key={index}>
-            <img src={post.fileUrl.length === 0 ? 'img/Sub1.jpeg' : post.fileUrl} alt='postImg' />
+          <div className='Post' id={post._id} key={index} onClick={() => handleClickPost(post._id)}>
+            <img src={post.fileUrl.length === 0 ? 'img/Sub1.jpeg' : post.fileUrl[0]} alt='postImg' />
             <span className='PostDate'>{post.date}</span>
             <span className='PostTitle'>{post.title}</span>
             <span className='PostSummary'>{post.contents}</span>
@@ -15,12 +21,12 @@ const BlogPosts = ({ posts, isLatest }) => {
         ))}
       </>
     );
-  } else {
+  } else if (type === 'blog') {
     return (
       <>
         {posts.map((post, index) => (
-          <div className='BlogPost' key={index}>
-            <img src={post.fileUrl.length === 0 ? 'img/Sub2.jpeg' : post.file} alt='postImg' />
+          <div className='BlogPost' id={post._id} key={index} onClick={() => handleClickPost(post._id)}>
+            <img src={post.fileUrl.length === 0 ? 'img/Sub1.jpeg' : post.fileUrl[0]} alt='postImg' />
             <span className='BlogPostDate'>{post.date}</span>
             <span className='BlogPostTitle'>{post.title}</span>
             <span className='BlogPostSummary'>{post.contents}</span>
@@ -28,6 +34,19 @@ const BlogPosts = ({ posts, isLatest }) => {
         ))}
       </>
     );
+  } else {
+    return (
+      <>
+        {posts.map((wish, index) => (
+          <div className='BlogPost' id={wish._id} key={index} onClick={() => handleClickPost(wish._id)}>
+            <img src={wish.fileUrl.length === 0 ? 'img/Sub1.jpeg' : wish.fileUrl} alt='postImg' />
+            <span className='BlogPostDate'>{wish.date}</span>
+            <span className='BlogPostTitle'>{wish.title}</span>
+            <span className='BlogPostSummary'>{wish.contents}</span>
+          </div>
+        ))}
+      </>
+    )
   }
 }
 
